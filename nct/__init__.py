e09836ad09398bd103da17b6e7722805714116b3
chinatsu <lolexplode@gmail.com>
2018-05-10 07:53:58 +0000

    Include testing data in schema

--- nct/__init__.py
+++ nct/__init__.py
@@ -15,20 +15,3 @@ login = flask_login.LoginManager()
 login.init_app(app)
 
 from nct import errors, views # The different pages are defined in views.py
-
-
-def create_app(db_uri):
-    app = Flask(__name__)
-
-    app.config["SQLALCHEMY_DATABASE_URI"] = db_uri
-    app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = True
-    app.secret_key = "1234abcdef"
-
-    db = SQLAlchemy(app)
-
-    login = flask_login.LoginManager()
-    login.init_app(app)
-
-    from nct import views, errors
-
-    return app
